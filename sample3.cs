c558fca71dc0240406387009acc011605cca8330
hipertekst <92983129+JohnRheve@users.noreply.github.com>
2021-11-20 16:01:07 +0800

    Create sample3.cs

---
+++ sample3.cs
@@ -0,0 +1,11 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		string firstName = "John Rheve";
+		string lastName = "Abad";
+		Console.WriteLine(firstName + " " + lastName);
+	}
+}
