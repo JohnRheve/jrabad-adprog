a7c72beb248f808d03b3d4a10603a0c1a2561d13
hipertekst <92983129+JohnRheve@users.noreply.github.com>
2021-11-20 15:58:11 +0800

    Create sample2.cs

---
+++ sample2.cs
@@ -0,0 +1,12 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		string name = "John Rheve";
+ 					int age = 21;
+		 Console.WriteLine("Hi " + name);
+ 			Console.WriteLine("Your age is " + age);
+	}
+}
